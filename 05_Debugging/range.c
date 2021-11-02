89eaf563bf45ae6f5c5062978be9dc32c375fc5f
Layvis1998 <Layvis@mx>
2021-11-02 15:48:30 +0300

    Homework № 5, improved

--- 05_Debugging/range.c
+++ 05_Debugging/range.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main(int argc, char* argv[]) {
-  int start = 0;
-  int end = 1;
-  int step = 1;
-  if (argc == 1) {
+int main(int argc, char* argv[])
+{
+  int32_t start = 0;
+  int32_t end = 1;
+  int32_t step = 1;
+  if (argc == 1)
+  {
     printf("%s",
       "usage: range [start] [end] [step]\n"
       "start: range start\n"
       "end: range end\n"
       "step: range step\n"
-      "output depends on the number of arguments:\n"
-      "\t1: [0, 1, ..., end-1]\n"
-      "\t2: [start, start+1, ..., end-1]\n"
-      "\t3: [start, start+step, ..., end-1]\n"
+      "output depends on the number of arguments as follows:\n"
+      "    1 arg: [0, 1, ..., end-1]\n"
+      "    2 args: [start, start+1, ..., end-1]\n"
+      "    3 args: [start, start+step, ..., end-1]\n"
        );
     return 0;
   }
-  else if (argc == 2) {
+  else if (argc == 2)
     sscanf(argv[1], "%d", &end);
-  }
-  else {
+  else
+  {
     sscanf(argv[1], "%d", &start);
     sscanf(argv[2], "%d", &end);
-    if (argc == 4) {
+    if (argc == 4) 
       sscanf(argv[3], "%d", &step);
-    }
     if (!step || start < end && step < 0)
       return 0;
   }
-  for (int i = start; i < end; i += step)
+  
+  for (int32_t i = start; i < end; i += step)
     printf("%d\n", i);
 }
