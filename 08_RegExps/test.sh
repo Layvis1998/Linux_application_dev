d848bfad3f381e74ce8e29e64b4319851ecf1a2e
Layvis1998 <Layvis@mx>
2021-12-07 00:05:59 +0300

    Not needed data

--- 08_RegExps/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ESUB=./esub
-regexp="$1"
-substitution="$2"
-string="$3"
-
-
-diff <(echo  "$string"  | sed -E "s/${regexp}/${substitution}/") <(${ESUB} "${regexp}" "${substitution}" "${string}")
